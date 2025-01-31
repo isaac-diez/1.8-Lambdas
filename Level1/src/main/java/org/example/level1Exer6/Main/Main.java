8082ff65b9f43a6ed4cd4b956c45a6fbdd34970a
Isaac Diez <isaac.diez@proton.me>
2025-01-31 18:36:35 +0100

    feat: Level 1 Exer 6: Sort list of numbers and strings by string length
    - Created a list containing numbers and strings.
    - Sorted the list so that strings are ordered from shortest to longest.
    - Printed the sorted list to verify the results.

---
+++ Level1/src/main/java/org/example/level1Exer6/Main/Main.java
@@ -0,0 +1,23 @@
+package org.example.level1Exer6.Main;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Object> stringAndIntegersList = Arrays.asList(42, "apple", "banana", 100, "kiwi", "cherry", "fig", "grape", 7);
+
+        List<String> filteredList = stringAndIntegersList.stream()
+                .filter(obj -> obj instanceof String)
+                .map(obj -> (String) obj )
+                .sorted(Comparator.comparingInt(String::length))
+                .toList();
+
+        filteredList.forEach(System.out::println);
+
+        }
+
+
+    }
