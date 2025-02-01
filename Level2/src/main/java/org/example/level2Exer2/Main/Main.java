35a1ae22616b191c2dea4969b160cd6edd9270b5
Isaac Diez <isaac.diez@proton.me>
2025-02-01 19:09:52 +0100

    Feat: started exercise.

---
+++ Level2/src/main/java/org/example/level2Exer2/Main/Main.java
@@ -0,0 +1,13 @@
+package org.example.level2Exer2.Main;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> integerList = Arrays.asList(3, 55, 44);
+
+
+    }
+}
