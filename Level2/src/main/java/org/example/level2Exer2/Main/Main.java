a3f11f670450a49a65d7b4b4cb34f76bd294f5d2
Isaac Diez <isaac.diez@proton.me>
2025-02-03 11:45:51 +0100

    Feat: Level 2 Exer 2: Format integer list with even/odd prefixes
    - Implemented a method with lambda expression that converts a list of `Integer` into a comma-separated string.
    - Numbers are prefixed with "e" if even and "o" if odd.
    - Example: Input `[3, 55, 44]` → Output `"o3, o55, e44"`.
    - Printed the formatted result.

--- Level2/src/main/java/org/example/level2Exer2/Main/Main.java
+++ Level2/src/main/java/org/example/level2Exer2/Main/Main.java
@@ -2,12 +2,18 @@ package org.example.level2Exer2.Main;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
         List<Integer> integerList = Arrays.asList(3, 55, 44);
 
+        String output = integerList.stream()
+                .map(integer -> (integer % 2 == 0 ? "e" : "o") + integer)
+                .collect(Collectors.joining(","));
+
+        System.out.println(output);
 
     }
 }
